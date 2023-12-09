6feb6d158e1102144ad5bc7d71d8db6ad1268540
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-10 00:09:04 +1100

    Add devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "events#index"
-
+  
+  devise_for :users
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

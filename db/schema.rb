69f9c385fd56fa3ee4c1965e8d1004eabb612ed1
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-10 00:43:53 +1100

    Add creator_id field to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_130801) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_09_133551) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_130801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end

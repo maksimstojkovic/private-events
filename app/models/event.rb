76c029fd516a177fb62fea78765a096d6a598d10
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-11 00:31:03 +1100

    Fix event attendee associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   
   has_many :event_attendees #, foreign_key: :event
-  has_many :attendees, through: :event_attendees
+  has_many :attendees, through: :event_attendees, source: :attendee, class_name: "User"
 end

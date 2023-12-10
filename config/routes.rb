ae9cc858d9c145466bbaf044b15fc6ae27bc4545
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-10 21:58:20 +1100

    Change event attendees route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "users/:id", to: "users#show", as: "profile"
   
   resources :events
-  resources :event_attendees
+  resources :event_attendees, path: "attend", as: "attend"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end

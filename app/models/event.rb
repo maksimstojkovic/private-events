21075e7576f526f69dc05f0d19fda6c67bb99cf3
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-11 23:17:13 +1100

    Refactor past and upcoming events into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
   
   has_many :event_attendees #, foreign_key: :event
   has_many :attendees, through: :event_attendees, source: :attendee, class_name: "User"
+
+  scope :past_events, -> { where(date: ...(DateTime.now)) }
+  scope :upcoming_events, -> { where(date: (DateTime.now)...) }
 end
